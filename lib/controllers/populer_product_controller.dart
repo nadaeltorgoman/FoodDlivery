7f7a90de35f16b2862691cb876d6312032f91b86
nadaeltorgoman <nada.khalid.eltorgoman@gmail.com>
2023-09-18 03:57:57 +0300

    - && + buttons in recommended page is updated tostop reducing lower than 0

--- lib/controllers/populer_product_controller.dart
+++ lib/controllers/populer_product_controller.dart
@@ -50,6 +50,10 @@ class PopularProductController extends GetxController {
     if ((_inCartItem + quantity) < 0) {
       Get.snackbar("Item Count", "You can't reduce more !",
           backgroundColor: AppColors.mainColor, colorText: Colors.white);
+      if (_inCartItem > 0) {
+        _quantity = -_inCartItem;
+        return _quantity;
+      }
       return 0;
     } else if ((_inCartItem + quantity) > 20) {
       Get.snackbar("Item Count", "You can't add more !",
