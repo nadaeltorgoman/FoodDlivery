e0101b4f6cb1f3686818d59f952f63522998475e
nadaeltorgoman <nada.khalid.eltorgoman@gmail.com>
2023-10-30 01:27:27 +0200

    manage some problems

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery_app/controllers/cart_controller.dart';
 import 'package:food_delivery_app/controllers/populer_product_controller.dart';
 import 'package:food_delivery_app/controllers/recommended_product_controller.dart';
-import 'package:food_delivery_app/pages/home/main_food_page.dart';
-import 'package:food_delivery_app/pages/splash/splash_page.dart';
 import 'package:food_delivery_app/route/route_helper.dart';
 import 'package:get/get.dart';
 import 'helper/depandencies.dart' as dep;
