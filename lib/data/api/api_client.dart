55b6b615ecc42cd82fcdb1fe4edbc2303498c054
nadaeltorgoman <nada.khalid.eltorgoman@gmail.com>
2023-10-29 23:42:32 +0200

    add 10.0.2.2:8000 as server id , edited timeout from 30 to 300 in api_client.dart , add permission to androidManifest.xml

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -3,16 +3,11 @@ import 'package:get/get.dart';
 
 class ApiClient extends GetConnect implements GetxService {
   late String token;
-  late Map<String, String> _mainHeaders;
   final String appBaseUrl;
   ApiClient({required this.appBaseUrl}) {
     baseUrl = appBaseUrl;
-    timeout = Duration(seconds: 30);
+    timeout = const Duration(seconds:300);
     token = AppConstants.TOKEN;
-    _mainHeaders = {
-      'Content-type': 'application/json; charset=UTF-8',
-      'Authorization': 'Bearer $token',
-    };
   }
   Future<Response> getData(String uri) async {
     try {
--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -4,7 +4,7 @@ class AppConstants {
   static const String APP_NAME = "DBFood";
   static const int APP_URL = 1;
 
-  static const String BASE_URL = "http://mvs.bslmeiyu.com";
+  static const String BASE_URL = "http://10.0.2.2:8000";
   static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
   static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended";
   static const String UPLOAD_URL = "/uploads/";
