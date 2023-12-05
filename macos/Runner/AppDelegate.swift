b36de3e9e4459e1ef4915c0000d15ed6fdc179e9
Nada Eltorgoman <nada.khalid.eltorgoman@gmail.com>
2023-12-05 04:05:30 +0200

    Edited AndroidManifest.xml , AppDelegate.swift with adding API keys

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,9 +1,15 @@
-import Cocoa
-import FlutterMacOS
+import UIKit
+import Flutter
+import GoogleMaps
 
-@NSApplicationMain
-class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyCSODCWa-uCdUv9ZpoUsK9yWKN7ntjT44E")
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
